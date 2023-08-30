3384051db0123e023dd5915409f1c676795f5f6e
ioloEJ42 <ioloevans42@gmail.com>
2023-08-30 20:40:14 +0100

    THeme

--- index.js
+++ index.js
@@ -37,3 +37,24 @@ searchInput.addEventListener("keypress", function onEvent(event) {
         document.getElementById("search-button").click();
     }
 });
+
+
+// DARK THEME TOGGLE MADE FROM https://lukelowrey.com/css-variable-theme-switcher/
+var toggle = document.getElementById("theme-toggle");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
